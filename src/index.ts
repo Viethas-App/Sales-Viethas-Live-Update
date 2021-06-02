c6b62b83fc0945bf4faa21fd81ac6ca7eb1ee88f
truonghuusang99 <truonghuusang99@gmail.com>
2021-06-02 16:36:02 +0700

    Update complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { app, Menu } from "electron";
+import { app, Menu, ipcMain, BrowserWindow } from "electron";
 import { createCapacitorElectronApp } from "@capacitor-community/electron";
+import { autoUpdater } from "electron-updater";
+import { log } from "electron-log";
 
+let updater;
+autoUpdater.autoDownload = false
 // The MainWindow object can be accessed via myCapacitorApp.getMainWindow()
 const myCapacitorApp = createCapacitorElectronApp({
   mainWindow: {
@@ -8,21 +12,84 @@ const myCapacitorApp = createCapacitorElectronApp({
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some Electron APIs can only be used after this event occurs.
-app.on("ready", () => {
-  myCapacitorApp.init();
-  Menu.setApplicationMenu(null)
+let win: BrowserWindow;
+function createDefaultWindow() {
+  return new Promise((resolve) => {
+    win = new BrowserWindow({
+      height: 400,
+      width: 400,
+      resizable: false,
+      minimizable: false,
+      maximizable: false,
+      titleBarStyle: "hiddenInset",
+      show: true,
+      parent: myCapacitorApp.getMainWindow(),
+      webPreferences: {
+        nodeIntegration: true
+      }
+
+    });
+    win.setMenu(null)
+    win.setTitle("")
+
+    win.loadURL(`file://${__dirname}/version.html`)
+    win.webContents.on('dom-ready', () => {
+      win.show();
+      ipcMain.on("updateAction", function (event, data) {
+        switch (data) {
+          case "success": autoUpdater.quitAndInstall(); break;
+          case "start": autoUpdater.downloadUpdate(); break;
+          default: win.close(); break;
+        }
+      })
+
+      resolve(win)
+    });
+
+    win.on('closed', () => {
+      win = null;
+    });
+  });
+}
+
+
+
+autoUpdater.on('checking-for-update', () => {
+  //log('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  //log('Update available.');
+  createDefaultWindow().then(() => {
+    win.webContents.send('message', info)
+  });
+})
+autoUpdater.on('update-not-available', (info) => {
+  // log('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  log('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Tốc độ tải xuống: " + Math.round(progressObj.bytesPerSecond / (1024 * 1024)) + "MB/s";
+  log_message = log_message + ' - Đã tải xuống ' + Math.round(progressObj.percent) + '%';
+  log_message = log_message + ' (' + Math.round(progressObj.transferred / (1024 * 1024)) + "MB/" + Math.round(progressObj.total / (1024 * 1024)) + 'MB)';
+  // log(log_message);
+  win.webContents.send('download_progress', log_message)
+})
+autoUpdater.on('update-downloaded', (info) => {
+  // log('Update downloaded');
+  win.webContents.send('download_progress', 'success')
 });
 
-// Quit when all windows are closed.
-app.on("window-all-closed", function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  //if (process.platform !== "darwin") {
+
+app.on('ready', function () {
+  myCapacitorApp.init()
+  // Create the Menu
+  Menu.setApplicationMenu(null);
+  autoUpdater.checkForUpdates();
+});
+app.on('window-all-closed', () => {
   app.quit();
-  //}
 });
 
 app.on("activate", function () {
@@ -31,4 +98,3 @@ app.on("activate", function () {
   if (myCapacitorApp.getMainWindow().isDestroyed()) myCapacitorApp.init();
 });
 
-// Define any IPC or other custom functionality below here
